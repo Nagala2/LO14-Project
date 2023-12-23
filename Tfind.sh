e096ded917a0492bdd0ea8a22c1a3bd7fbeb8a0e
Maël <mael.javer_kala@utt.fr>
2023-12-24 00:10:13 +0100

    add customdiff and modify tfinds

--- Tfind.sh
+++ Tfind.sh
@@ -1,9 +1,13 @@
-
-
 liste=$(find $1)
 rm A
 for i in $liste
 do
 	dh=$(stat -c "%y" $i | cut -f 1-2 -d " ")
-	echo $i $dh >>A
+	echo $i
+	if [ -d $i ]
+	then
+		echo $i d >>A
+	else
+		echo $i $dh >>A
+	fi
 done
